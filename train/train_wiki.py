a2277782867825c0942ee8769e55134eab3ea22f
nobodyzxc <dici86.chen@gmail.com>
2019-06-25 05:14:14 +0800

    update train_wiki

--- train/train_wiki.py
+++ train/train_wiki.py
@@ -8,14 +8,16 @@ from gensim.models.word2vec import PathLineSentences
 from gensim.models.callbacks import CallbackAny2Vec
 from logger import EpochLogger
 
+line_count = 341018
+
 sentences = PathLineSentences("wiki_tokens")
 
 print("creating model...")
-model = Word2Vec(tqdm(sentences, total=1023054), min_count=5, size=250, window=5, workers=3, callbacks=[EpochLogger()])
+model = Word2Vec(tqdm(sentences, total=line_count), min_count=5, size=250, window=5, workers=3, callbacks=[EpochLogger()])
 
 print("training model...")
 #model.train(tqdm(sentences), total_examples=len(sentences), epochs=150, callbacks=[EpochLogger()])
-model.train(tqdm(sentences, total=1023054), total_examples=model.corpus_count, epochs=150, callbacks=[EpochLogger()])
+model.train(tqdm(sentences, total=line_count), total_examples=model.corpus_count, epochs=150, callbacks=[EpochLogger()])
 
 print("saving model...")
 model.save("./wiki_{}.w2v".format(datetime.datetime.now().strftime("%Y%m%d_%H%M")))
