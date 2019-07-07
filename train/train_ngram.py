01646a7ae56b96558356b362d976bab155d2b426
nobodyzxc <dici86.chen@gmail.com>
2019-07-08 00:24:29 +0800

    update

--- train/train_ngram.py
+++ train/train_ngram.py
@@ -10,29 +10,15 @@ from logger import EpochLogger
 def retain_chinese(line):
     return re.compile(r"[^\u4e00-\u9fa5]").sub('', line).replace('臺', '台')
 
-class EpochLogger(CallbackAny2Vec):
-    '''Callback to log information about training'''
-
-    def __init__(self):
-        self.epoch = 0
-
-    def on_epoch_begin(self, model):
-        if self.epoch == 0:
-            self.bar = tqdm(total=model.epochs)
-
-    def on_epoch_end(self, model):
-        self.bar.update(1)
-        self.epoch += 1
-        if self.epoch == model.epochs:
-            self.bar.close()
-            self.bar = None
-
 titleJson = os.path.join('..', 'data', "title.json")
 stopwordFile = os.path.join('..', 'data', "StopWord.txt")
 tokenFile = os.path.join('..', 'tokens', 'search_dict_token.txt')
 tokeyFile = os.path.join('..', 'tokens', 'search_dict_tokey.txt')
 
-pretrained_model = os.path.join('wiki_20190625_1042.w2v')
+bitokenFile = os.path.join('..', 'tokens', 'bigram_token.txt')
+bitokeyFile = os.path.join('..', 'tokens', 'bigram_tokey.txt')
+tritokenFile = os.path.join('..', 'tokens', 'trigram_token.txt')
+tritokeyFile = os.path.join('..', 'tokens', 'trigram_tokey.txt')
 
 cut_method = jieba.cut_for_search
 queryDictFile = os.path.join('..', 'data', 'dict.txt')
@@ -44,6 +30,11 @@ print("opening file...")
 
 docs = trim(open(tokenFile).read().split('\n'))#[:301]#[:5000]
 docIDs = trim(open(tokeyFile).read().split('\n'))#[:301]#[:5000]
+bitoken = trim(open(bitokenFile).read().split('\n'))#[:5000]#[:301]
+#bitokey = trim(open(bitokeyFile).read().split('\n'))#[:5000]#[:301]
+tritoken = trim(open(tritokenFile).read().split('\n'))#[:5000]#[:301]
+#tritokey = trim(open(tritokeyFile).read().split('\n'))#[:5000]#[:301]
+
 stopwords = open(stopwordFile, 'r').read().split()
 titles = json.load(open(titleJson, "r"))
 
@@ -58,14 +49,22 @@ for i, key in enumerate(tqdm(docIDs)):
             in cut_method(title) if w not in stopwords]))
         docs[i] += title_token
 
+# add ngram
+for i, bitok in enumerate(bitoken):
+    docs[i] += ' ' + bitok
+
+for i, tritok in enumerate(tritoken):
+    docs[i] += ' ' + tritok
+
+
 print("spliting tokens...")
 docsTokens = [doc.split() for doc in tqdm(docs)]
 
-print("loading pretrained model...")
-model = Word2Vec.load(pretrained_model)
+print("creating model...")
+model = Word2Vec(tqdm(docsTokens), min_count=5, size=200, window=5, workers=3, callbacks=[EpochLogger()])
 
 print("training model...")
 model.train(tqdm(docsTokens), total_examples=len(docsTokens), epochs=100, callbacks=[EpochLogger()])
 
 print("saving model...")
-model.save("./mix_{}.w2v".format(datetime.datetime.now().strftime("%Y%m%d_%H%M")))
+model.save("./{}.w2v".format(datetime.datetime.now().strftime("%Y%m%d_%H%M")))
