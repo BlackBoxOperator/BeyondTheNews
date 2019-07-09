b538b1d7365b0f45aa80db196f57ce88f53413ea
nobodyzxc <dici86.chen@gmail.com>
2019-07-09 17:53:47 +0800

    merge mcustom_query.txt to 8.py

--- model/8.py
+++ model/8.py
@@ -10,7 +10,47 @@ from score_functions import twostage
 from itertools import starmap
 from bm25 import BM25Transformer
 
-queryFile = os.path.join('..', 'data', 'custom_query.txt')
+QS_1 = """通姦 刑法 除罪 除罪化
+機車 二段 段式 二段式 左轉 待轉
+博弈 特區 台灣 合法 合法化
+中華 航空 空服員 罷工 合理
+性交 交易 性交易 合法 合法化
+ECFA 早收 清單 達到 預期 成效 海峽 兩岸 經濟
+減免 證所稅
+中油 觀塘 興建 第三 天然 天然氣 接收 接收站
+中國 學生 納入 健保
+臺灣 中小 中小學 含 高職 專科 服儀 規定 含 髮 襪 鞋 給予 學生 自主
+使用 加密 貨幣
+學雜費 調漲
+政府 舉債 發展 前瞻 建設 計畫
+電競 列入 體育 競技
+台鐵 東移 徵收 徵收案
+陳 前 總統 保外 就醫 水扁 扁
+年金 改革  公教 軍公教 月退 優存 利率 十八 趴
+動物 實驗
+油價 凍漲 緩漲
+旺旺 中時 併購 中嘉
+通姦 刑法 應該 除罪 除罪化 同意 贊成 支持
+應該 取消 機車 強制 二段 段式 二段式 左轉 待轉 同意 贊成 支持
+支持 博弈 特區 台灣 合法 合法化 同意 贊成
+中華 航空 空服員 罷工 合理
+性交 交易 性交易 應該 合法 合法化 同意 贊成 支持
+ECFA 早收 清單 達到 預期 成效 海峽 兩岸 經濟
+應該 減免 證所稅 同意 贊成 支持
+贊成 中油 觀塘 興建 第三 天然 天然氣 接收 接收站
+支持 中國 學生 納入 健保 應該 同意 贊成
+支持 臺灣 中小 中小學 含 高職 專科 服儀 規定 含 髮 襪 鞋 給予 學生 自主 應該 同意 贊成
+不 支持 使用 加密 貨幣 不 應該 不 同意 不 贊成 反對
+不 支持 學雜費 調漲 不 應該 不 同意 不 贊成 反對
+同意 政府 舉債 發展 前瞻 建設 計畫 應該 贊成 支持
+支持 電競 列入 體育 競技 應該 贊成 同意
+反對 台鐵 東移 徵收 徵收案 不 支持 不 同意 不 應該
+支持 陳 前 總統 保外 就醫 應該 同意 贊成 水扁 扁
+年金 改革 應 取消 或應 調降 公教 軍公教 月退 優存 利率 十八 趴
+同意 動物 實驗 贊成 支持 應該
+油價 應該 凍漲 緩漲 贊成 支持
+反對 旺旺 中時 併購 中嘉 不 應該 不 支持 不 贊成"""
+
 stopwordFile = os.path.join('..', 'data', "StopWord.txt")
 outputFile = os.path.join('..', 'submit', 'current.csv')
 
@@ -24,7 +64,7 @@ jieba.load_userdict(queryDictFile)
 if __name__ == '__main__':
 
     stopwords = open(stopwordFile, 'r').read().split()
-    queries = open(queryFile, 'r').read().split('\n')
+    queries = QS_1.split('\n')
     two_stage_queries = [(i + 1, f, s) for i, (f, s)
                                 in enumerate(zip(queries[:20], queries[20:]))]
 
